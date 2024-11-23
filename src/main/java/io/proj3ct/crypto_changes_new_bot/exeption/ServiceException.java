401ab53b88d65d61e043a0f88e8462a0bc0c8bc3
Bogdanov Boris <bogdboris@gmail.com>
2024-11-24 01:44:01 +0500

    Первый коммит\

---
+++ src/main/java/io/proj3ct/crypto_changes_new_bot/exeption/ServiceException.java
@@ -0,0 +1,12 @@
+package io.proj3ct.crypto_changes_new_bot.exeption;
+
+public class ServiceException extends Exception {
+
+    public ServiceException(String message) {
+        super(message);
+    }
+
+    public ServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
